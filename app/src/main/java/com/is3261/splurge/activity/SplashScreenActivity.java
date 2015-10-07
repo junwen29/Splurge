5de3c62b2b6329f7a52b0d244d7943520df57c7d
junwen29 <junwen2992@gmail.com>
2015-10-07 23:03:16 +0800

    change launcher activity to splashscreen

---
+++ app/src/main/java/com/is3261/splurge/activity/SplashScreenActivity.java
@@ -0,0 +1,69 @@
+package com.is3261.splurge.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.is3261.splurge.R;
+import com.is3261.splurge.helper.OwnerStore;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    public enum SessionState {
+        LOGGED_OUT,
+        LOGGED_IN,
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SessionState sessionState = getSessionState();
+
+        if (sessionState == SessionState.LOGGED_IN){
+            goToMain();
+        }
+        else {
+            startActivity(new Intent (this, FrontPageActivity.class));
+        }
+        //TODO create splash screen with just app logo
+        setContentView(R.layout.activity_splash_screen);
+        setup();
+
+    }
+
+    private void goToMain() {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    private void setup(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private SessionState getSessionState() {
+        OwnerStore ownerStore = new OwnerStore(this);
+        if (ownerStore.getAuthToken() == null) {
+            return SessionState.LOGGED_OUT;
+        }
+        else {
+            return SessionState.LOGGED_IN;
+        }
+    }
+
+}
